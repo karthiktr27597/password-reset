6282f5fd4f223072794167db4cdfeede3e05c459
karthiktr27597 <karthiktr27597@gmail.com>
2023-07-20 01:04:34 +0530

    first

--- src/Components/LoginSuccess/index.js
+++ src/Components/LoginSuccess/index.js
@@ -1,19 +1,19 @@
-import axios from "axios";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+// import axios from "axios";
+// import { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+// import { useEffect } from "react";
 
 const LoginSuccess = (props) => {
 
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0YjZhY2QxMzAyOGRlMDBiNDExYTI0NyIsImlhdCI6MTY4OTc5MTM5MCwiZXhwIjoxNjkyMzgzMzkwfQ.PrvLLBalSKoDIKQRJTr94HSXS0gMCQ2jiX6k6t29-iA"
+    // const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0YjZhY2QxMzAyOGRlMDBiNDExYTI0NyIsImlhdCI6MTY4OTc5MTM5MCwiZXhwIjoxNjkyMzgzMzkwfQ.PrvLLBalSKoDIKQRJTr94HSXS0gMCQ2jiX6k6t29-iA"
 
     //const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0YWZkNTkwMmNkNDkyNGExZTkzYWRjOSIsImlhdCI6MTY4OTI0NTEwNywiZXhwIjoxNjkxODM3MTA3fQ.y516XKFytFJP1AWWah4bfjn_aQ50EPN5Dqho9TJQ97w"
 
-    const config = {
-        headers: { 'x-auth-token': token }
-    };
+    // const config = {
+    //     headers: { 'x-auth-token': token }
+    // };
 
     // const headers = {
     //     "Content-Type": "text/json"
@@ -25,27 +25,27 @@ const LoginSuccess = (props) => {
 
     // const navigate = useNavigate();
 
-    const authCheck = async (config) => {
-        try {
-            console.log("data", config);
-            const response = await axios.post('https://password-reset-vbrg.onrender.com/home', config)
-            // response.data.headers['Content-Type'];
+    // const authCheck = async (config) => {
+    //     try {
+    //         console.log("data", config);
+    //         const response = await axios.post('https://password-reset-vbrg.onrender.com/home', config)
+    //         // response.data.headers['Content-Type'];
 
-            console.log(response);
-            if (response.status = 200) {
-                console.log('Authorized user');
-                return
-            }
-        }
-        catch (err) {
-            console.log(err);
-            navigate("/");
-        }
-    }
+    //         console.log(response);
+    //         if (response.status = 200) {
+    //             console.log('Authorized user');
+    //             return
+    //         }
+    //     }
+    //     catch (err) {
+    //         console.log(err);
+    //         navigate("/");
+    //     }
+    // }
 
-    useEffect(() => {
-        authCheck(config);
-    }, [])
+    // useEffect(() => {
+    //     authCheck(config);
+    // }, [])
 
 
     return (
